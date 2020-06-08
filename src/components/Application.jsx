98bf7015667a37d6831d6f6a3c16f37a99948526
alex.paclin <alex.paclin@gmail.com>
2020-06-08 23:36:10 +0500

    6. Adding a Post

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,40 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
 import Posts from './Posts';
+import { collectIdsAndDocs } from '../utilities';
 import { firestore } from '../firebase';
 
 const Application = () => {
 	const initialState = {
-		posts: [
-			{
-				id: '1',
-				title: 'A Very Hot Take',
-				content:
-					'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!',
-				user: {
-					uid: '123',
-					displayName: 'Bill Murray',
-					email: 'billmurray@mailinator.com',
-					photoURL: 'https://www.fillmurray.com/300/300',
-				},
-				stars: 1,
-				comments: 47,
-			},
-			{
-				id: '2',
-				title: 'The Sauciest of Opinions',
-				content:
-					'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!',
-				user: {
-					uid: '456',
-					displayName: 'Mill Burray',
-					email: 'notbillmurray@mailinator.com',
-					photoURL: 'https://www.fillmurray.com/400/400',
-				},
-				stars: 3,
-				comments: 0,
-			},
-		],
+		posts: [],
 	};
 
 	const [state, setState] = useState(initialState);
@@ -43,17 +15,18 @@ const Application = () => {
 	useEffect(() => {
 		const getSnapshotAsync = async () => {
 			const snapshot = await firestore.collection('posts').get();
-			const posts = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+			const posts = snapshot.docs.map(collectIdsAndDocs);
 			setState({ posts });
 		};
 		getSnapshotAsync();
 	}, []);
 
-	const handleCreate = (post) => {
-		setState({ ...state, posts: [post, ...posts] });
+	const handleCreate = async (post) => {
+    const docRef = await firestore.collection('posts').add(post);
+    const doc = await docRef.get();
+    const newPost = collectIdsAndDocs(doc);
+		setState({ ...state, posts: [newPost, ...posts] });
 	};
-
-	//const { posts } = state;
 
 	return (
 		<main className="Application">
