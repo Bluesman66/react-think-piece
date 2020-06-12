66e89189bb7bb66f1828a83292b28cfdb680fd48
alex.paclin <alex.paclin@gmail.com>
2020-06-12 20:36:02 +0500

    4. Creating a User Document

--- src/firebase.js
+++ src/firebase.js
@@ -4,18 +4,42 @@ import 'firebase/auth';
 import firebase from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB7TlaopJ49iFwu_Y90he84BnVN6xvCfzM",
-  authDomain: "pai-think-piece.firebaseapp.com",
-  databaseURL: "https://pai-think-piece.firebaseio.com",
-  projectId: "pai-think-piece",
-  storageBucket: "pai-think-piece.appspot.com",
-  messagingSenderId: "734428543358",
-  appId: "1:734428543358:web:40bd2228314d3b5ff09b2f"
+	apiKey: 'AIzaSyB7TlaopJ49iFwu_Y90he84BnVN6xvCfzM',
+	authDomain: 'pai-think-piece.firebaseapp.com',
+	databaseURL: 'https://pai-think-piece.firebaseio.com',
+	projectId: 'pai-think-piece',
+	storageBucket: 'pai-think-piece.appspot.com',
+	messagingSenderId: '734428543358',
+	appId: '1:734428543358:web:40bd2228314d3b5ff09b2f',
 };
 
 firebase.initializeApp(firebaseConfig);
 
 window.firebase = firebase;
+
+export const createUserProfileDocument = async (user, additionalData) => {
+	if (!user) return;
+	// Get a reference to the place in the database where a user profile might be
+	const userRef = firestore.doc(`users/${user.id}`);
+	// Go and fetch the document from that location
+	const snapshot = await userRef.get();
+
+	if (!snapshot.exists) {
+		const { displayName, email, photoURL } = user;
+		const createdAt = new Date();
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				photoURL,
+				createdAt,
+				...additionalData,
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	}
+};
 
 export const firestore = firebase.firestore();
 export const auth = firebase.auth();
